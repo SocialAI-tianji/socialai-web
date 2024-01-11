107317136b72aa534a0d2d21fcccda140d1dafdc
Tomo Wang <tomo_wang@163.com>
2024-01-11 20:40:18 +0800

    feat: sample menu use shadcn component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <div className='relative min-h-screen'>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   )
--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -1,7 +1,34 @@
+"use client"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export default function Header() {
   return (
-    <header>
-      Header
+    <header className="top-0 left-0 w-full py-5 text-white bg-black sticky transition-all duration-300">
+      <div className="container mx-auto flex">
+        <nav className="flex gap-4">
+          <h1 className=""><a href="/">Social AI</a></h1>
+          <DropdownMenu>
+            <DropdownMenuTrigger>Research</DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-black text-white">
+              <DropdownMenuItem>Overview</DropdownMenuItem>
+              <DropdownMenuItem>Index</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger>API</DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-black text-white">
+              <DropdownMenuItem>API</DropdownMenuItem>
+              <DropdownMenuItem>Data Privacy</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </nav>
+      </div>
     </header>
   )
 }
