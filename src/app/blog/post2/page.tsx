1bfae7eee35e03eaf82a420e4c3757185a973ddf
sanbuphy <physicoada@gmail.com>
2024-02-03 11:10:48 +0800

    fix page

---
+++ src/app/blog/post2/page.tsx
@@ -0,0 +1,109 @@
+export default function post2() {
+    return <div className='bg-black text-white'>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+            <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+              <div className='w-full md:w-1/2 order-2 md:order-1 mt-10 md:mt-0'>
+                <figure className='relative pl-4'>
+                  <blockquote className='text-2xl md:text-6xl'>
+                    <p>
+                    使先进AI系统与人类价值观保持一致 - 观点与优先事项
+                    </p>
+                  </blockquote>
+                  <blockquote className='text-2xl md:text-2xl'>
+                    <p>
+                    
+                    </p>
+                  </blockquote>
+                  
+                  {/* <figcaption className='mt-6'>
+                    <span className='font-semibold block'>Lilian Weng</span>
+                    <span className='block'>Applied AI at OpenAI</span>
+                  </figcaption> */}
+                </figure>
+              </div>
+              <div className='w-full md:w-1/2 order-1 md:order-2'>
+                <img alt="" src={`../images/post2.png`}
+                  className='w-full h-full object-cover aspect-square'></img>
+              </div>
+            </div>
+      </div>
+      <div className='bg-white text-black'>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+        <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+          <div className='w-full md:w-1/4 mt-10 md:mt-0'>
+            <figure className='relative pl-4'>
+              <blockquote >
+                <h6>January 21, 2024</h6>   
+              </blockquote>
+            </figure>
+          </div>
+          <div className='w-full md:w-1/2 '>
+            <blockquote >
+              <p className='text-2xl md:text-2xl'>使先进AI系统与人类价值观保持一致 - 观点与优先事项</p>   
+            </blockquote>              
+            <p className='mt-16'>
+              简介 <br></br>
+              <br/>
+              人工智能(AI)系统继续表现出令人印象深刻的进步,从自然语言处理到计算机视觉等。一些专家预测AI最终可能会超过人类智力,并对经济和社会产生转型性影响。但是,随着能力的增长,与人类优先事项保持一致变得越来越重要。AI校准是确保先进系统通过理解、明确和实现共享价值观来促进人类 flourishing 的挑战。<br></br>
+              <br/>
+              校准不到位会带来许多危险,包括内置偏见、不公平、不透明的决策、蓄意滥用,或者简单地优化AI系统朝着开发者或社会没有预期的目标。正如 Dame Wendy Hall 指出的:“最紧迫的问题是如何确保机器学习驱动的AI系统的设计和操作与社会价值观和道德规范兼容。”<br></br>
+              <br/>
+              本研究旨在阐明各利益相关者对AI校准人类价值观优先事项的看法。它通过定量和定性方法来探索视角范围,以揭示校准的共识和分歧领域。这可以告知政策和机制的制定,这对于AI能力和影响力不断增强是必要的。 <br></br>
+              <br/>
+              文献综述 <br></br>
+              <br/>
+              各种框架已被提出关于价值校准,源自道德哲学、经济学、法学和计算机科学等学科。关键基础包括Rawl的“无知之幕”,要求公平对待所有受影响的群体;阿西莫夫的机器人三大法则,旨在确保安全性;以及像价值学习等范式,强调不断更新目标。 <br></br>
+              <br/>
+              然而,当前并不存在单一占主导地位的校准框架,且在技术、伦理和社会方法方面仍有许多悬而未决的问题。例如,谁的价值观应该构成校准的框架 - 开发者、企业、政府、跨国机构还是最终用户? 如何协调价值的文化相关性和主观性与机器实现所需的客观性? <br></br>
+              <br/>
+              本研究弥补了收集和比较不同群体校准观点以确定凝聚力和冲突领域的空白。它引出了各方面态度的因素,如透明度、问责制、隐私、安全性、人类控制、能力限制等文献中强调的优先事项,如就业自动化影响或武器开发。 <br></br>
+              <br/>
+              方法论 <br></br>
+              <br/>
+              采用Mixed方法,结合定量调查与定性深度访谈。参与者跨背景,包括AI安全部研究者、开发人员/工程师、决策者、伦理学家、律师和最终用户。 <br></br>
+              <br/>
+              调查采用5点李克特量表的问题评估20个从文献中提取的价值的重要性,以定量分析偏好。访问的代码识别关键主题和值校准方法。这有助于三角验证定性和定量结果。 <br></br>
+              <br/>
+              结果 <br></br>
+              <br/>
+              调查分析发现,透明度、问责制、避免偏见和人类监督等核心原则存在共识。然而,在合适的功能极限、价值更新机制和经济框架方面存在分歧。相比其他群体,工程师们更看重精确性、递归性和功能广度,而最终用户和决策者们则强调限制与人类控制保护。 <br></br>
+              <br/>
+              访谈强化了这些偏好,同时也揭示出组内的细微差别。例如,一些工程师采用长期的社会视角权衡功能,而其他人则专注于技术挑战。经济考量因素范围从繁荣最大化到基本收入促进等。 <br></br>
+              <br/>
+              讨论 <br></br>
+              <br/>
+              结果解释了人类价值的主观性与系统设计的客观性需求之间的紧张关系。它表明需要允许参与受影响群体意见的程序与专家指导相结合,以解决权衡取舍。创造性的政策和治理系统对可持续积极的人类参与具有重要意义。 <br></br>
+              <br/>
+              虽然样本和方法可以扩展,但本研究表明,为了匹配社会期望与快速的AI进步,需要广泛参与。建议包括独立监督机构、专门法院、许可框架和公共基础设施监测。 <br></br>
+              <br/>
+              结论 <br></br>
+              <br/>
+              实现先进AI的潜力同时避免陷阱需要从一开始解决复杂的价值校准问题。本研究总结了当前的想法,并揭示出共识领域以及不确定性。它强调需要包容性、想象力和迭代更新的方法,以确保未来AI技术能够促进所有人的繁荣。            </p>   
+            {/* <p className='text-2xl md:text-2xl mt-16'>
+              Preventing abuse
+            </p>
+            <p className='mt-4'>
+
+              <img alt="" src={`../images/stangel-2022-0484.jpg`}
+                className='w-full h-full object-cover aspect-square'></img>
+
+              <br/>
+
+            </p>
+            <figcaption className='mt-6'>
+            <span className='font-semibold block'>Authors</span>
+            <span className='block'>Lilian Weng</span>
+            </figcaption>  */}
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+        <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+        Related research
+        </div>
+      </div>
+    </div>
+    
+    
+  }
---
+++ src/app/blog/post6/page.tsx
@@ -0,0 +1,64 @@
+export default function post5() {
+    return <div className='bg-black text-white'>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+            <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+              <div className='w-full md:w-1/2 order-2 md:order-1 mt-10 md:mt-0'>
+                <figure className='relative pl-4'>
+                  <blockquote className='text-2xl md:text-6xl'>
+                    <p>
+                    代办
+                    </p>
+                  </blockquote>
+
+                  
+
+                </figure>
+              </div>
+
+            </div>
+      </div>
+      <div className='bg-white text-black'>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+        <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+          <div className='w-full md:w-1/4 mt-10 md:mt-0'>
+            <figure className='relative pl-4'>
+              <blockquote >
+                <h6>January XXXX</h6>   
+              </blockquote>
+            </figure>
+          </div>
+          <div className='w-full md:w-1/2 '>
+            <blockquote >
+              <p className='text-2xl md:text-2xl'>代办</p>   
+            </blockquote>              
+            <p className='mt-16'>
+              引言 <br></br>
+            </p>   
+            {/* <p className='text-2xl md:text-2xl mt-16'>
+              Preventing abuse
+            </p>
+            <p className='mt-4'>
+
+              <img alt="" src={`../images/stangel-2022-0484.jpg`}
+                className='w-full h-full object-cover aspect-square'></img>
+
+              <br/>
+
+            </p>
+            <figcaption className='mt-6'>
+            <span className='font-semibold block'>Authors</span>
+            <span className='block'>Lilian Weng</span>
+            </figcaption>  */}
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="container mx-auto -mt-20 pt-20 md:-mt-16 md:pt-16">
+        <div className='flex flex-col md:flex-row items-start justify-start border-t-[1px] border-[rgba(255,255,255,.2)] pt-4 pb-0 md:pb-16'>
+        Related research
+        </div>
+      </div>
+    </div>
+    
+    
+  }
